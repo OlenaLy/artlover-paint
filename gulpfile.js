64699ba485bd80606ba780f56cc2c01999ad9776
Olena Lytvynenko <lena.litvinen4ik@gmail.com>
2026-01-18 14:43:14 +0200

    gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -7,39 +7,67 @@ import uglify from 'gulp-uglify'
 import rename from 'gulp-rename'
 
 const sass = gulpSass(dartSass)
+
 const paths = {
-    styles: {
-        src: './src/style/**/*.scss', 
-        main: './src/style/main.scss', 
-        dest: './build/css'
-    },
-    scripts: {
-        src: './src/script/**/*.js', 
-        main: './src/script/app.js', 
-        dest: './build/js'
-    }
-};
-function buildStyles(){
-    return gulp.src(paths.styles.main)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({ cascade: false }))
-        .pipe(cleanCSS({ level: 2 }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest(paths.styles.dest));
+  styles: {
+    src: './src/style/**/*.scss',
+    main: './src/style/main.scss',
+    dest: './build/css'
+  },
+  scripts: {
+    src: './src/script/**/*.js',
+    main: './src/script/app.js',
+    dest: './build/js'
+  },
+  html: {
+    src: './index.html',
+    dest: './build'
+  },
+  assets: {
+    src: './assets/**/*',
+    dest: './build/assets'
+  }
+}
+
+function buildStyles() {
+  return gulp.src(paths.styles.main)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(cleanCSS({ level: 2 }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.styles.dest))
 }
 
 function minifyJs() {
-    return gulp.src(paths.scripts.main, { allowEmpty: true })
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest(paths.scripts.dest));
+  return gulp.src(paths.scripts.main, { allowEmpty: true })
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.scripts.dest))
+}
+
+function copyHtml() {
+  return gulp.src(paths.html.src)
+    .pipe(gulp.dest(paths.html.dest))
+}
+
+function copyAssets() {
+  return gulp.src(paths.assets.src)
+    .pipe(gulp.dest(paths.assets.dest))
 }
 
 function watchFiles() {
-    gulp.watch(paths.styles.src, buildStyles);
-    gulp.watch(paths.scripts.src, minifyJs);
+  gulp.watch(paths.styles.src, buildStyles)
+  gulp.watch(paths.scripts.src, minifyJs)
+  gulp.watch(paths.html.src, copyHtml)
+  gulp.watch(paths.assets.src, copyAssets)
 }
 
-export const build = gulp.series(buildStyles, minifyJs);
-export const watch = gulp.series(buildStyles, watchFiles);
-export default gulp.series(build, watch);
+export const build = gulp.series(
+  buildStyles,
+  minifyJs,
+  copyHtml,
+  copyAssets
+)
+
+export const watch = gulp.series(build, watchFiles)
+export default gulp.series(build, watch)
--- src/script/app.js
+++ src/script/app.js
@@ -87,26 +87,26 @@ if (glideEl) {
 const cardsAll = document.querySelector('.sec3-cards__slides');
 
 const images = [
-  'assets/images/1.jpg',
-  'assets/images/2.jpg',
-  'assets/images/3.jpg',
-  'assets/images/4.jpg',
-  'assets/images/5.jpg',
-  'assets/images/6.jpg',
-  'assets/images/7.jpg',
-  'assets/images/8.jpg',
-  'assets/images/9.jpg',
-  'assets/images/10.jpg',
-  'assets/images/11.jpg',
-  'assets/images/12.jpg',
-  'assets/images/13.jpg',
-  'assets/images/14.jpg',
-  'assets/images/15.jpg',
-  'assets/images/16.jpg',
-  'assets/images/17.jpg',
-  'assets/images/18.jpg',
-  'assets/images/19.jpg',
-  'assets/images/20.jpg'
+  './assets/images/1.jpg',
+  './assets/images/2.jpg',
+  './assets/images/3.jpg',
+  './assets/images/4.jpg',
+  './assets/images/5.jpg',
+  './assets/images/6.jpg',
+  './assets/images/7.jpg',
+  './assets/images/8.jpg',
+  './assets/images/9.jpg',
+  './assets/images/10.jpg',
+  './assets/images/11.jpg',
+  './assets/images/12.jpg',
+  './assets/images/13.jpg',
+  './assets/images/14.jpg',
+  './assets/images/15.jpg',
+  './assets/images/16.jpg',
+  './assets/images/17.jpg',
+  './assets/images/18.jpg',
+  './assets/images/19.jpg',
+  './assets/images/20.jpg'
 ];
 
 images.forEach((src, index) => {
