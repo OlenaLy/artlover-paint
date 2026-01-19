7927861b6d5ff0bbe0f8de4a97c4fe3b64da297f
Olena Lytvynenko <lena.litvinen4ik@gmail.com>
2026-01-19 13:00:25 +0200

    version 1.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,10 @@ import autoprefixer from 'gulp-autoprefixer'
 import cleanCSS from 'gulp-clean-css'
 import uglify from 'gulp-uglify'
 import rename from 'gulp-rename'
+import browserSync from 'browser-sync';
 
-const sass = gulpSass(dartSass)
+const sass = gulpSass(dartSass);
+const bs = browserSync.create();
 
 const paths = {
   styles: {
@@ -36,6 +38,7 @@ function buildStyles() {
     .pipe(cleanCSS({ level: 2 }))
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest(paths.styles.dest))
+    .pipe(bs.stream());
 }
 
 function minifyJs() {
@@ -43,31 +46,37 @@ function minifyJs() {
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(bs.stream());
 }
 
 function copyHtml() {
   return gulp.src(paths.html.src)
     .pipe(gulp.dest(paths.html.dest))
+    .pipe(bs.stream());
 }
 
 function copyAssets() {
-  return gulp.src(paths.assets.src)
-    .pipe(gulp.dest(paths.assets.dest))
+  return gulp.src('assets/**/*', {
+    base: '.',
+    encoding: false
+  })
+  .pipe(gulp.dest('build'));
 }
 
-function watchFiles() {
+function serve() {
+  bs.init({
+    server: {
+      baseDir: './build'
+    },
+    port: 3000
+  });
+
   gulp.watch(paths.styles.src, buildStyles)
   gulp.watch(paths.scripts.src, minifyJs)
   gulp.watch(paths.html.src, copyHtml)
   gulp.watch(paths.assets.src, copyAssets)
 }
 
-export const build = gulp.series(
-  buildStyles,
-  minifyJs,
-  copyHtml,
-  copyAssets
-)
-
-export const watch = gulp.series(build, watchFiles)
-export default gulp.series(build, watch)
+export const build = gulp.series(buildStyles, minifyJs, copyHtml, copyAssets);
+export const dev = gulp.series(build, serve);
+export default dev;
